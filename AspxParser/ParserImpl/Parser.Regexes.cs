8f70f6dc5a49aa04133b1059fae5317a4d97166a
Ivan Kochurkin <kvanttt@gmail.com>
2018-03-02 18:00:33 +0300

    Use just regex strings for Parser regexes.

--- AspxParser/ParserImpl/Parser.Regexes.cs
+++ AspxParser/ParserImpl/Parser.Regexes.cs
@@ -1,25 +1,71 @@
-﻿using System.Text.RegularExpressions;
-using System.Web.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
+using static System.Text.RegularExpressions.RegexOptions;
 
 namespace AspxParser
 {
     partial class Parser
     {
         private readonly Regex tagRegex;
-        private static readonly Regex tagRegex35 = new TagRegex35();
-        private static readonly Regex tagRegex40 = new TagRegex();
-        private static readonly Regex directiveRegex = new DirectiveRegex();
-        private static readonly Regex endtagRegex = new EndTagRegex();
-        private static readonly Regex aspCodeRegex = new AspCodeRegex();
-        private static readonly Regex aspExprRegex = new AspExprRegex();
-        private static readonly Regex aspEncodedExprRegex = new AspEncodedExprRegex();
-        private static readonly Regex databindExprRegex = new DatabindExprRegex();
-        private static readonly Regex commentRegex = new CommentRegex();
-        private static readonly Regex includeRegex = new IncludeRegex();
-        private static readonly Regex textRegex = new TextRegex();
-        private static readonly Regex gtRegex = new GTRegex();
-        private static readonly Regex ltRegex = new LTRegex();
-        private static readonly Regex serverTagsRegex = new ServerTagsRegex();
-        private static readonly Regex runatServerRegex = new RunatServerRegex();
+
+        private static readonly Regex tagRegex35 = new Regex(
+            "\\G<(?<tagname>[\\w:\\.]+)(\\s+(?<attrname>\\w[-\\w:]*)(\\s*=\\s*\"(?<attrval>[^\"]*)\"|\\s*=\\s*'(?<attrval>[^']*)'|\\s*=\\s*(?<attrval><%#.*?%>)|\\s*=\\s*(?<attrval>[^\\s=/>]*)|(?<attrval>\\s*?)))*\\s*(?<empty>/)?>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex tagRegex40 = new Regex(
+            "\\G<(?<tagname>[\\w:\\.]+)(\\s+(?<attrname>\\w[-\\w:]*)(\\s*=\\s*\"(?<attrval>[^\"]*)\"|\\s*=\\s*'(?<attrval>[^']*)'|\\s*=\\s*(?<attrval><%#.*?%>)|\\s*=\\s*(?<attrval>[^\\s=\"'/>]*)|(?<attrval>\\s*?)))*\\s*(?<empty>/)?>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex directiveRegex = new Regex(
+            "\\G<%\\s*@(\\s*(?<attrname>\\w[\\w:]*(?=\\W))(\\s*(?<equal>=)\\s*\"(?<attrval>[^\"]*)\"|\\s*(?<equal>=)\\s*'(?<attrval>[^']*)'|\\s*(?<equal>=)\\s*(?<attrval>[^\\s\"'%>]*)|(?<equal>)(?<attrval>\\s*?)))*\\s*?%>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex endtagRegex = new Regex(
+            "\\G</(?<tagname>[\\w:\\.]+)\\s*>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex aspCodeRegex = new Regex(
+            "\\G<%(?!@)(?<code>.*?)%>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex aspExprRegex = new Regex(
+            "\\G<%\\s*?=(?<code>.*?)?%>",
+             Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex aspEncodedExprRegex = new Regex(
+            "\\G<%:(?<code>.*?)?%>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex databindExprRegex = new Regex(
+            "\\G<%#(?<encode>:)?(?<code>.*?)?%>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex commentRegex = new Regex(
+            "\\G<%--(([^-]*)-)*?-%>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex includeRegex = new Regex(
+            "\\G<!--\\s*#(?i:include)\\s*(?<pathtype>[\\w]+)\\s*=\\s*[\"']?(?<filename>[^\\\"']*?)[\"']?\\s*-->",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex textRegex = new Regex(
+            "\\G[^<]+",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex gtRegex = new Regex(
+            "[^%]>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex ltRegex = new Regex(
+            "<[^%]",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex serverTagsRegex = new Regex(
+            "<%(?![#$])(([^%]*)%)*?>",
+            Compiled | Multiline | Singleline, TimeSpan.FromTicks(-10000L));
+
+        private static readonly Regex runatServerRegex = new Regex(
+            "runat\\W*server",
+            Compiled | IgnoreCase | Multiline | Singleline | CultureInvariant, TimeSpan.FromTicks(-10000L));
     }
 }
